08693548912b32f5bbda1512abf9fcaf2dbccef2
Mella <melladjs@hotmail.com>
2021-06-02 17:06:05 +0200

    creacion de header, footer y nuevo componente carrusel

--- instawatch/src/app/app.module.ts
+++ instawatch/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarruselComponent } from './componentes/carrusel/carrusel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarruselComponent
   ],
   imports: [
     BrowserModule,
