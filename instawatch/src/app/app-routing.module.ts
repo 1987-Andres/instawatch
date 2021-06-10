c5fcdc70257a75c5cff22c7f3ef66cd3d516e78e
Mella <melladjs@hotmail.com>
2021-06-10 16:06:56 +0200

    ajustes anchos web

--- instawatch/src/app/app-routing.module.ts
+++ instawatch/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CarruselComponent } from './componentes/carrusel/carrusel.component';
 import { HomeComponent } from './componentes/home/home.component';
 import { LoginComponent } from './componentes/login/login.component';
 import { RegistroComponent } from './componentes/registro/registro.component';
