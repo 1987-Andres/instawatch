2deaa96a7ea6ea0e3a6fe2e2586b95fc4824c3e9
Mella <melladjs@hotmail.com>
2021-06-10 17:59:10 +0200

    Creacion de contacto y retoques css

--- instawatch/src/app/app-routing.module.ts
+++ instawatch/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
 import { HomeComponent } from './componentes/home/home.component';
 import { LoginComponent } from './componentes/login/login.component';
 import { RegistroComponent } from './componentes/registro/registro.component';
@@ -8,6 +9,7 @@ const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
   { path: 'registro', component: RegistroComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'contacto', component: ContactoComponent },
   { path: '**', redirectTo: '/home' },
 ];
 
