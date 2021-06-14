613964743da8926b192bc5a9a33b4b2c2951322f
Luis <luism.castellano@gmail.com>
2021-06-14 11:12:04 +0200

    creando rutas watch-card

--- instawatch/src/app/app-routing.module.ts
+++ instawatch/src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CarruselComponent } from './componentes/carrusel/carrusel.component';
 import { HomeComponent } from './componentes/home/home.component';
+import { WatchCardComponent } from './componentes/watch-card/watch-card.component';
+
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
   { path: 'carrusel', component: CarruselComponent },
   { path: '**', redirectTo: '/home' },
+  { path: 'watch-card', component: WatchCardComponent }
 ];
 
 @NgModule({
