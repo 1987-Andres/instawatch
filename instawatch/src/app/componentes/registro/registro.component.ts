bb3f36b4adca2c0edea359442c2ed5c3249e561c
Mella <melladjs@hotmail.com>
2021-06-14 09:53:50 +0200

    retoques formulario

--- instawatch/src/app/componentes/registro/registro.component.ts
+++ instawatch/src/app/componentes/registro/registro.component.ts
@@ -24,8 +24,15 @@ export class RegistroComponent implements OnInit {
       email: new FormControl('', [
         Validators.pattern(/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/)
       ]),
-      dni: new FormControl('', [
-        this.dniValidator
+      direccion: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+      sexo: new FormControl('', [
+        Validators.required
+      ]),
+      fecha_nacimiento: new FormControl('', [
+        Validators.required
       ]),
       password: new FormControl('', [
         Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/)
@@ -49,27 +56,6 @@ export class RegistroComponent implements OnInit {
 
   checkControl(controlName, validatorName) {
     return this.formulario.get(controlName).hasError(validatorName) && this.formulario.get(controlName).touched;
-  }
-
-  dniValidator(control) {
-
-    // const dni = control.value;
-    // DESTRUCTURING
-    const { value: dni } = control;
-    const relLetras = 'TRWAGMYFPDXBNJZSQVHLCKET'.split('');
-
-    if (/^\d{8}[a-zA-Z]$/.test(dni)) {
-      const numero = dni.substr(0, dni.length - 1);
-      const letra = dni.substr(dni.length - 1, 1);
-
-      if (letra.toUpperCase() !== relLetras[numero % 23]) {
-        return { dnivalidator: 'La letra no se corresponde' };
-      } else {
-        return null;
-      }
-    } else {
-      return { dnivalidator: 'El formato es incorrecto' };
-    }
   }
 
   passwordRepeatValidator(form) {
