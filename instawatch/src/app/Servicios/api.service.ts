a6cace43c00737c1e43c4b790ab9741be38ee0f9
Mella <melladjs@hotmail.com>
2021-06-16 17:02:23 +0200

    pruebas varias....funcionando todo menos las imagenes de los relojes

--- instawatch/src/app/Servicios/api.service.ts
+++ instawatch/src/app/Servicios/api.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Reloj } from '../componentes/interfaces/lista_relojes.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +9,26 @@ export class ApiService {
 
   constructor(private httpClient: HttpClient) { }
 
+  baseUrl = "http://localhost:3000/api/watches";
+
   getAll(): Promise<any> {
+
     // GET http://localhost:3000/watches
 
-    return this.httpClient.get('http://localhost:3000/api/watches').toPromise();
+    return this.httpClient.get(this.baseUrl).toPromise();
   }
+
+  // filterByMarca(pMarca: string): Reloj[] {
+  //   return RELOJES.filter((reloj) => {
+  //     return reloj.marca.toLowerCase() === pMarca.toLowerCase();
+  //   });
+  // }
+
+  // filterByletra(pLetra: string): Reloj[] {
+  //   return RELOJES.filter((reloj) => {
+  //     return reloj.marca.toLowerCase() === pLetra.toLowerCase()
+  //   });
+  // }
 
 
 }
--- instawatch/src/app/componentes/watch-show/watch-show.component.ts
+++ instawatch/src/app/componentes/watch-show/watch-show.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from 'src/app/Servicios/api.service';
-import { ListaRelojesService } from 'src/app/Servicios/lista-relojes.service';
 import { Reloj } from '../interfaces/lista_relojes.interface';
 
 
@@ -14,30 +13,30 @@ export class WatchShowComponent implements OnInit {
 
   relojes: Reloj[];
 
-  constructor(private activatedRoute: ActivatedRoute, private listaRelojesService: ListaRelojesService, private apiService: ApiService) { }
+  constructor(private activatedRoute: ActivatedRoute, private apiService: ApiService) { }
 
   ngOnInit(): void {
-    this.apiService.getAll().then(console.log);
-    this.relojes = this.listaRelojesService.getAll();
-
-
+    this.apiService.getAll().then(response => {
+      this.relojes = response;
+    })
+      .catch(error => console.log(error));
   }
 
-  onChange($event) {
-    if ($event.target.value === 'todos') {
-      this.relojes = this.listaRelojesService.getAll();
-    } else {
-      this.relojes = this.listaRelojesService.filterByMarca($event.target.value);
-    }
-  }
+  // onChange($event) {
+  //   if ($event.target.value === 'todos') {
+  //     this.relojes = this.api.getAll();
+  //   } else {
+  //     this.relojes = this.listaRelojesService.filterByMarca($event.target.value);
+  //   }
+  // }
 
 
-  onKeyUp($event) {
-    if ($event.target.value !== '') {
-      this.relojes = this.listaRelojesService.filterByletra($event.target.value);
-    } else {
-      this.relojes = this.listaRelojesService.getAll();
-    }
-  }
+  // onKeyUp($event) {
+  //   if ($event.target.value !== '') {
+  //     this.relojes = this.listaRelojesService.filterByletra($event.target.value);
+  //   } else {
+  //     this.relojes = this.listaRelojesService.getAll();
+  //   }
+  // }
 
 }
