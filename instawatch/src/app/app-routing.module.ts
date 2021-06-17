8dade844946c1530bcddf34aa293e79033014823
Mella <melladjs@hotmail.com>
2021-06-17 13:34:40 +0200

    creacion de rutas

--- instawatch/src/app/app-routing.module.ts
+++ instawatch/src/app/app-routing.module.ts
@@ -8,6 +8,10 @@ import { RegistroComponent } from './componentes/registro/registro.component';
 import { WatchCardComponent } from './componentes/watch-card/watch-card.component';
 import { WatchShowComponent } from './componentes/watch-show/watch-show.component';
 import { ForumComponent } from './componentes/forum/forum.component';
+import { SaludoComponent } from './componentes/forum/saludo/saludo.component';
+import { NewArrivalComponent } from './componentes/forum/new-arrival/new-arrival.component';
+import { CompraVentaComponent } from './componentes/forum/compra-venta/compra-venta.component';
+import { DudasComponent } from './componentes/forum/dudas/dudas.component';
 
 
 const routes: Routes = [
@@ -19,6 +23,10 @@ const routes: Routes = [
   { path: 'watch-card', component: WatchCardComponent },
   { path: 'watch-show', component: WatchShowComponent },
   { path: 'forum', component: ForumComponent },
+  { path: 'forum/saludo', component: SaludoComponent },
+  { path: 'forum/dudas', component: DudasComponent },
+  { path: 'forum/compraventa', component: CompraVentaComponent },
+  { path: 'forum/newarrival', component: NewArrivalComponent },
   { path: '**', redirectTo: '/home' },
 ];
 
